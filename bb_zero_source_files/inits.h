adbe3b14f52beb908f34516f1fc54de3c2d26b09
Whisnesky <jakub.wisniewski96@gmail.com>
2017-03-28 22:08:16 +0200

    Inits source files

---
+++ bb_zero_source_files/inits.h
@@ -0,0 +1,10 @@
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_conf.h"
+#include "misc.h"
+
+void initWheels();
+
+void RCC_Init();
+void PWM_TIM4_Init();
